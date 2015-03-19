1b05aecc3a44571e68a5e8869ba5b0f7a463715a
antoine <antoine@Antoine-PC>
2015-03-19 16:31:01 +0100

    Ajoute la fonction de balayage

--- src/graphic/ListeRectangle.java
+++ src/graphic/ListeRectangle.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
  *
  * @author Maxime
  */
-public class ListeRectangle extends ArrayList<Rectangle>{
+public class ListeRectangle extends ArrayList<Rectangle> {
 
     /**
      * Constructeur vide
@@ -19,14 +19,14 @@ public class ListeRectangle extends ArrayList<Rectangle>{
     public ListeRectangle() {
         super();
     }
-    
+
     public int toutesLesPaires() {
         // Initialisation
         int nbPaires = 0;
         // Parcours
-        for(int i=0;i<this.size();i++) {
-            for(int j=i+1;j<this.size();j++) {
-                if(comparer(this.get(i), this.get(j))) {
+        for (int i = 0; i < this.size(); i++) {
+            for (int j = i + 1; j < this.size(); j++) {
+                if (comparer(this.get(i), this.get(j))) {
                     nbPaires++;
                 }
             }
@@ -34,7 +34,38 @@ public class ListeRectangle extends ArrayList<Rectangle>{
         // Retour
         return nbPaires;
     }
-    
+
+    /**
+     * R: rectangle courant (donc Rx2: x2 de R) 
+     * RC: rectangles comparés (donc RCx1: x1 de RC)
+     * 
+     * Rappel du sujet:
+     * Soit R le rectangle courant. Balayer la liste L de rectangles dans l’ordre en partant de R et en
+     * s’arrêtant au premier segment, D, dont la coordonnée x du coin inférieur gauche est plus 
+     * grande que la coordonnée x du coin supérieur droit de R. Parmi les rectangles après R dans la 
+     * liste L, ceux qui coupent R en x sont exactement ceux après R et avant D.
+     */
+    public int balayage() {
+        // Initialisation
+        int nbPaires = 0;
+        // Parcours
+        for (int i = 0; i < this.size(); i++) {
+            int Rx2 = this.get(i).x2;
+            for (int j = i + 1; j < this.size(); j++) {
+                int RCx1 = this.get(j).x1;
+                if (Rx2 > RCx1) {
+                    if (comparerYseulement(this.get(i), this.get(j))) {
+                        nbPaires++;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        // Retour
+        return nbPaires;
+    }
+
     public boolean comparer(Rectangle r1, Rectangle r2) {
         //System.out.println(" *** COMPARAISON DE " + r1 + " ET " + r2 + " ***");
         // Test sur les X
@@ -42,16 +73,24 @@ public class ListeRectangle extends ArrayList<Rectangle>{
         //System.out.println("\tComparaison sur les X : " + bX);
         // Test sur les Y
         boolean bY = ((r1.y1 < r2.y1) && (r1.y1 > r2.y2)) || ((r2.y1 < r1.y1) && (r2.y1 > r1.y2));
-       // System.out.println("\tComparaison sur les Y : " + bY);
+        // System.out.println("\tComparaison sur les Y : " + bY);
         return bX && bY;
     }
-    
+
+    public boolean comparerYseulement(Rectangle r1, Rectangle r2) {
+        //System.out.println(" *** COMPARAISON DE " + r1 + " ET " + r2 + " ***");
+        // Test sur les Y
+        boolean bY = ((r1.y1 < r2.y1) && (r1.y1 > r2.y2)) || ((r2.y1 < r1.y1) && (r2.y1 > r1.y2));
+        // System.out.println("\tComparaison sur les Y : " + bY);
+        return bY;
+    }
+
     public ListeRectangle trierTableau() {
         ListeRectangle res = new ListeRectangle();
-        for(int i=0;i<this.size();i++) {
+        for (int i = 0; i < this.size(); i++) {
             int val = this.get(i).x1;
             int rang = chercherRang(res, val);
-            if(rang == -1) {
+            if (rang == -1) {
                 res.add(this.get(i));
             } else {
                 res.add(rang, this.get(i));
@@ -59,10 +98,10 @@ public class ListeRectangle extends ArrayList<Rectangle>{
         }
         return res;
     }
-    
+
     public int chercherRang(ListeRectangle liste, int val) {
-        for(int i=0;i<liste.size();i++) {
-            if(liste.get(i).x1 > val) {
+        for (int i = 0; i < liste.size(); i++) {
+            if (liste.get(i).x1 > val) {
                 return i;
             }
         }
